514bbc330020117070fea957b923e98af2d5f31b
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-05-28 07:15:54 -0400

    Se refactorizo sendGreeting al añadir una funcion que envia el mensaje de Cumpleaños

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -1,6 +1,3 @@
-import fs from "fs";
-import path from "path";
-import { Employee } from "./Employee";
 import { EmployeesRepository } from "./EmployeesRepository";
 
 export class BirthdayService {
@@ -9,7 +6,10 @@ export class BirthdayService {
   sendGreetings(ourDate, fileName, smtpUrl, smtpPort, transport) {
     let employeesRepository = new EmployeesRepository();
     const employees= employeesRepository.getEmployeesFromFile(ourDate,fileName);
-    
+    this.sendBirthdayMessage(employees,smtpUrl, smtpPort, transport);
+  }
+
+  sendBirthdayMessage(employees,smtpUrl, smtpPort, transport){
     employees.forEach((employee) => {
       const message = {
         host: smtpUrl,
